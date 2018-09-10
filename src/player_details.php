528e05a67844f61687b3cb495d64f4adfb111be2
Akshit Monga <akshit.monga@gmail.com>
2018-09-09 20:10:32 -0400

    Player details page

---
+++ src/player_details.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="css/Style_Teamname.css" rel="stylesheet">
+</head>
+
+<body>
+<div id="bg">
+  <img src="http://img.skitch.com/20120905-fccfjyqxpxbkrhs6wct3xuyrjf.png" alt="">
+</div>
+
+<form method="POST" action="player_details_res.php">
+  <div class="title">Enter Player ID</div>
+  &nbsp;
+  <input type="text" name ="player" placeholder="Enter Player ID ">
+
+  &nbsp;
+  <input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html>
