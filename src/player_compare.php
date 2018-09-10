68367f3032267cd0f7dcf9037dcea26911309086
Akshit Monga <akshit.monga@gmail.com>
2018-09-09 20:09:52 -0400

    Player Compare Page

---
+++ src/player_compare.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="css/Style_Teamname.css" rel="stylesheet">
+</head>
+
+<body>
+<div id="bg">
+  <img src="http://img.skitch.com/20120905-fccfjyqxpxbkrhs6wct3xuyrjf.png" alt="">
+</div>
+
+<form method="POST" action="pcompare_results.php">
+  <div class="title">Player Compare</div>
+  &nbsp;
+  <input type="text" name ="pid1" placeholder="Enter Player Id of First Player ">
+  <input type="text" name ="pid2" placeholder="Enter Player Id of Second Player ">
+  &nbsp;
+  <input type="submit" name="submit" value="Compare">
+</form>
+
+</body>
+</html>
