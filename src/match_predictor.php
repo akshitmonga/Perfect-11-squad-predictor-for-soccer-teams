6f76342027aa89316255044d96422b5b12c749b5
Akshit Monga <akshit.monga@gmail.com>
2018-09-09 20:08:25 -0400

    Match Predictor Request Page

---
+++ src/match_predictor.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="css/Style_Teamname.css" rel="stylesheet">
+</head>
+
+<body>
+<div id="bg">
+  <img src="http://img.skitch.com/20120905-fccfjyqxpxbkrhs6wct3xuyrjf.png" alt="">
+</div>
+
+<form method="POST" action="match_pred_res.php">
+  <div class="title">Match Predictor</div>
+  &nbsp;
+  <input type="text" name ="tid1" placeholder="Enter Team Id of First Team ">
+  <input type="text" name ="tid2" placeholder="Enter Team Id of Second Team ">
+  &nbsp;
+  <input type="submit" name="submit" value="Compare">
+</form>
+
+</body>
+</html>
