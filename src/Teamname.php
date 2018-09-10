f82faaa39d037df912b0b5282b863d83b392f61c
Akshit Monga <akshit.monga@gmail.com>
2018-09-09 20:15:22 -0400

    Find Team Info Page

---
+++ src/Teamname.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="css/Style_Teamname.css" rel="stylesheet">
+</head>
+
+<body>
+<div id="bg">
+  <img src="http://img.skitch.com/20120905-fccfjyqxpxbkrhs6wct3xuyrjf.png" alt="">
+</div>
+
+<form method="POST" action="temp.php">
+  <div class="title">TEAM NAME</div>
+  <input type="text" name ="tname" placeholder="Enter Team name ">
+  <input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html>
