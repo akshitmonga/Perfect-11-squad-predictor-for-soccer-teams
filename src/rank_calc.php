fc08f99737554badf2a4212915db788b165076e4
Akshit Monga <akshit.monga@gmail.com>
2018-09-09 20:11:31 -0400

    Rank calculate page

---
+++ src/rank_calc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="css/Style_Teamname.css" rel="stylesheet">
+</head>
+
+<body>
+<div id="bg">
+  <img src="http://img.skitch.com/20120905-fccfjyqxpxbkrhs6wct3xuyrjf.png" alt="">
+</div>
+
+<form method="POST" action="rank_calc_res.php">
+  <div class="title">Player ranking on the basis of salary</div>
+  &nbsp;
+  <input type="text" name ="manager" placeholder="Enter Manager ID ">
+
+  &nbsp;
+  <input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html>
