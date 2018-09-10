dcd252cac7cd40611e5025a4f7a225decc2d0bb6
Akshit Monga <akshit.monga@gmail.com>
2018-09-09 20:05:08 -0400

    Delete Player page

---
+++ src/drop_player.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="css/Style_Teamname.css" rel="stylesheet">
+</head>
+
+<body>
+<div id="bg">
+  <img src="http://img.skitch.com/20120905-fccfjyqxpxbkrhs6wct3xuyrjf.png" alt="">
+</div>
+
+<form method="POST" action="drop_player_res.php">
+  <div class="title">Enter details</div>
+  &nbsp;
+  <input type="text" name ="player" placeholder="Enter Player ID ">
+  <input type="text" name ="manager" placeholder="Enter Manager ID ">
+
+  &nbsp;
+  <input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html>
