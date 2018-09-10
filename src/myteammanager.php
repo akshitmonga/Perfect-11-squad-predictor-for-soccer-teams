1f4eca49c6873c5753f8b1da2252e6a3bd6ad2da
Akshit Monga <akshit.monga@gmail.com>
2018-09-09 20:09:11 -0400

    Find Manager Page

---
+++ src/myteammanager.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="css/Style_Teamname.css" rel="stylesheet">
+</head>
+
+<body>
+<div id="bg">
+  <img src="http://img.skitch.com/20120905-fccfjyqxpxbkrhs6wct3xuyrjf.png" alt="">
+</div>
+
+<form method="POST" action="myteammanager_res.php">
+  <div class="title">Enter Manager ID</div>
+  &nbsp;
+  <input type="text" name ="manager" placeholder="Enter Manager ID ">
+
+  &nbsp;
+  <input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html>
